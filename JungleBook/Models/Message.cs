86daea48d038dd870a7883ac03c06b2dc91c2e18
jrapkin <jrap1243@gmail.com>
2020-04-17 19:51:42 -0500

    Updated git ignore to ignore, Added Messages class to be used with mimekit emails.

---
+++ JungleBook/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JungleBook.Models
+{
+	public class Message
+    {
+        public int MessageId { get; set; }
+        [Display(Name = "From")]
+        public string SenderAddress { get; set; }
+        [Display(Name = "To")]
+        public string RecipientAddress { get; set; }
+        [Display(Name = "Message")]
+        public string MessageBody { get; set; }
+    }
+}
