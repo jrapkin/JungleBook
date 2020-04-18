d44e31385f6db8b0193dbac3952896a7ac59981e
jrapkin <jrap1243@gmail.com>
2020-04-17 19:35:37 -0500

    Added MimeKit to JungleBook project. Added MailKit class which will serve to send invitations to people to sign up as a user to collaborate on trip planning.  Added CreateEmail method which takes in a string name, string from, a list of type string recipients and a string message. It will return out an overloaded CreateEmail method.

---
+++ JungleBook/Features/MailKit.cs
@@ -0,0 +1,24 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JungleBook.Features
+{
+	public static class MailKit
+	{
+		public static MimeMessage CreateEmail(string name, string from, List<string> listOfRecipients, string messageText)
+		{
+			MailboxAddress senderAddress = new MailboxAddress(name, from);
+			List<MailboxAddress> recipientAddresses = new List<MailboxAddress>();
+			foreach(string address in listOfRecipients)
+			{
+				recipientAddresses.Add(new MailboxAddress(name, address));
+			}
+			return CreateEmail(senderAddress, recipientAddresses, messageText);
+		}
+
+	}
+}
