92d32409edb78495a0c4fe26560ab6a639fb8580
jrapkin <jrap1243@gmail.com>
2020-04-22 16:33:22 -0500

    Added a list property(selectedDestinations) and multiselectlist property (DestinationOptions)to the trip view model. Changed using model on the CreateTrip view to use TripViewModel, and to show a list box.

--- JungleBook/Models/ViewModels/TripViewModel.cs
+++ JungleBook/Models/ViewModels/TripViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +8,15 @@ namespace JungleBook.Models.ViewModels
 {
 	public class TripViewModel
 	{
+		public TripViewModel()
+		{
+		}
 		public Traveler TravelerLoggedIn { get; set; }
 		public List<Traveler> TravelBuddies { get; set; }
 		public UserProfile UserProfile { get; set; }
+		public Trip Trip { get; set; }
 		public List<DayActivity> DayActivities { get; set; }
+		public List<int> selectedDestinations { get; set; }
+		public MultiSelectList DestinationOptions { get; set; }
 	}
 }
