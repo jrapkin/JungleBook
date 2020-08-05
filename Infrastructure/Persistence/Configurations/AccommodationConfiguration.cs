c21a589df8642a2a3c23b6c1a78f36c0ec159018
jrapkin <jrap1243@gmail.com>
2020-08-05 10:38:23 -0500

    Created configuration files for accommodation and activity entity models.

---
+++ Infrastructure/Persistence/Configurations/AccommodationConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Infrastructure.Persistence.Configurations
+{
+	public class AccommodationConfiguration : IEntityTypeConfiguration<Accommodation>
+	{
+		public void Configure(EntityTypeBuilder<Accommodation> builder)
+		{
+			builder.HasKey(e => e.AccommodationId);
+			builder.Property(e => e.Name)
+				.HasColumnName("Name");
+
+			builder.OwnsOne(e => e.Address);
+		}
+	}
+}
---
+++ Infrastructure/Persistence/Configurations/ActivityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Persistence.Configurations
+{
+	public class ActivityConfiguration : IEntityTypeConfiguration<Activity>
+	{
+		public void Configure(EntityTypeBuilder<Activity> builder)
+		{
+			builder.HasKey(e => e.ActivityId);
+
+			builder.Property(e => e.Name)
+				   .HasColumnName("Name");
+
+			builder.Property(e => e.Description)
+				   .HasColumnName("Description");
+
+			builder.OwnsOne(e => e.Address);
+
+		}
+	}
+}
